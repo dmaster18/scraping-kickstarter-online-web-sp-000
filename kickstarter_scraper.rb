f82bcde52f5fef104fc2a51762a66e768b177769
David Strauch <dmaster18@gmail.com>
2019-09-18 19:20:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,3 +15,4 @@ binding.pry
 #image link: project.css("div.project-thumbnail a img").attribute("src").value
 #description: project.css("p.bbcard_blurb").text
 #location: project.css("ul.project_meta span.location-name").value.text
+#percent_funded: project.css("ul")
