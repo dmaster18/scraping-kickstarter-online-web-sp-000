2a815d62923921a40ad33f2dfdf672d57cd29e4b
David Strauch <dmaster18@gmail.com>
2019-09-18 19:28:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,6 +12,8 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
       :image_link => 
+      :description =>
+      :location =>
       
       
       
